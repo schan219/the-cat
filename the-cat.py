384ae7331ba2dda044717cc68fa00364e33a7406
Stephanie Chan <stephaniechan219@gmail.com>
2017-01-28 22:17:41 -0800

    left-join

--- the-cat.py
+++ the-cat.py
@@ -1,1 +1,22 @@
-#hahahahaa
+"""
+chance of student finishing the MOOC
+Training data: Climate
+Testing data: China
+"""
+
+import pandas as pd
+import os
+
+PERSON_COURSE_CLEANED = "../UBCx__Climate101x__3T2015_cleaned/person_course_cleaned.tsv"
+PERSON_COURSE_DAY_CLEANED = "../UBCx__Climate101x__3T2015_cleaned/person_course_day_cleaned.tsv"
+
+topdir = os.getcwd()
+user_path = os.path.join(topdir, PERSON_COURSE_CLEANED)
+person_course_day_cleaned_path = os.path.join(topdir, PERSON_COURSE_DAY_CLEANED)
+
+user_df = pd.read_table(user_path)
+person_course_day_cleaned_df = pd.read_table(person_course_day_cleaned_path)
+
+result = user_df.join(person_course_day_cleaned_df, lsuffix='user_id', rsuffix='user_id')
+
+print(result)
