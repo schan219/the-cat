4e4fad17692bdd7d2a3c62999804f246526fd2ad
Stephanie Chan <stephaniechan219@gmail.com>
2017-01-28 22:24:09 -0800

    run to convert left-join to csv file

--- the-cat.py
+++ the-cat.py
@@ -6,6 +6,7 @@ Testing data: China
 
 import pandas as pd
 import os
+import csv
 
 PERSON_COURSE_CLEANED = "../UBCx__Climate101x__3T2015_cleaned/person_course_cleaned.tsv"
 PERSON_COURSE_DAY_CLEANED = "../UBCx__Climate101x__3T2015_cleaned/person_course_day_cleaned.tsv"
@@ -19,4 +20,7 @@ person_course_day_cleaned_df = pd.read_table(person_course_day_cleaned_path)
 
 result = user_df.join(person_course_day_cleaned_df, lsuffix='user_id', rsuffix='user_id')
 
-print(result)
+with open('left-join.csv', 'w') as csvfile:
+    a = csv.writer(csvfile)
+    a.writerows(result)
+    csvfile.close()
