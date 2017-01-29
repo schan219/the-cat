2b713005c8e65288182486ecdb8f3ca110bb0fca
Stephanie Chan <stephaniechan219@gmail.com>
2017-01-29 11:57:09 -0800

    drop nproblems_attempted column from climate

--- the-cat.py
+++ the-cat.py
@@ -17,14 +17,14 @@ PERSON_COURSE_DAY_CLEANED_CHINA = "../UBCx__China300_1x__3T2015_cleaned/person_c
 
 def get_data(data_path1, data_path2, filename):
     topdir = os.getcwd()
-    person_course_cleaned_path = os.path.join(topdir, data_path1)
-    person_course_day_cleaned_path = os.path.join(topdir, data_path2)
+    data_path1 = os.path.join(topdir, data_path1)
+    data_path2 = os.path.join(topdir, data_path2)
 
-    person_course_cleaned_df = pd.read_table(person_course_cleaned_path)
-    person_course_day_cleaned_df = pd.read_table(person_course_day_cleaned_path)
+    data1_df = pd.read_table(data_path1)
+    data2_df = pd.read_table(data_path2)
 
-    #result = person_course_cleaned_df.join(person_course_day_cleaned_df, lsuffix='user_id', rsuffix='user_id')
-    result = pd.merge(person_course_cleaned_df, person_course_day_cleaned_df, on='user_id', how = 'inner')
+    result = pd.merge(data1_df, data2_df, on='user_id', how='inner')
+
     def get_grade_label(grade):
         '''
         Return 1 if course was completed (non-NA value), 0 otherwise.
@@ -47,19 +47,19 @@ def get_data(data_path1, data_path2, filename):
     (because if we only keep the rows that don't have NAs we only get 5 datapoints :'()
     '''
     result = result.fillna(0)
-    # result.to_csv('left-join.tsv', sep="\t")
 
     '''
     Since we don't want to normalize the grades, and want to use it as the labels instead, we store them separately
     '''
     grades = result['grade']
     result = result.drop(['grade', 'user_id'], axis=1)
+    if 'nproblems_answered' in result:
+        result = result.drop(['nproblems_answered'], axis=1)
 
     '''
     Normalize the remaining numerical data: set it to have mean of 0 and standard deviation of 1.
     '''
     result_norm = result.apply(lambda x: (x - np.mean(x)) / np.std(x))
-    # result_norm.to_csv('result-norm.tsv', sep="\t")
 
     frames = [result_norm, grades]
     final_result = pd.concat(frames, axis=1)
