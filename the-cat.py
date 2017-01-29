907d59749511e8058651c17111552f692609babc
Stephanie Chan <stephaniechan219@gmail.com>
2017-01-28 23:30:00 -0800

    minor changes

--- the-cat.py
+++ the-cat.py
@@ -2,11 +2,11 @@
 chance of student finishing the MOOC
 Training data: Climate
 Testing data: China
+SKLearn Preprocessing Normalize: Avg_Dt
 """
 
 import pandas as pd
 import os
-import csv
 
 PERSON_COURSE_CLEANED = "../UBCx__Climate101x__3T2015_cleaned/person_course_cleaned.tsv"
 PERSON_COURSE_DAY_CLEANED = "../UBCx__Climate101x__3T2015_cleaned/person_course_day_cleaned.tsv"
@@ -20,6 +20,6 @@ person_course_day_cleaned_df = pd.read_table(person_course_day_cleaned_path)
 
 result = user_df.join(person_course_day_cleaned_df, lsuffix='user_id', rsuffix='user_id')
 
-print(result)
+print(result[1,1])
 
 result.to_csv('left-join.tsv', sep="\t")
